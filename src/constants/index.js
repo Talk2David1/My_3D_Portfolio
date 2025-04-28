4e34b87abfacf78cbb46078f2105885b6c235c24
David Ezekiel <david@axumtechnologies.com>
2025-04-28 17:38:00 +0100

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,255 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    typescript,
+    figma,
+    whatsapp
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: motion,
+        name: "Motion",
+        type: "Animation",
+    },
+    {
+        imageUrl: mui,
+        name: "Material-UI",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nextjs,
+        name: "Next.js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React js",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: typescript,
+        name: "TypeScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: figma,
+        name: "Figma",
+        type: "Design", 
+    },
+];
+
+export const experiences = [
+    {
+        title: "Full stack Developer",
+        company_name: "Axum Technologies",
+        icon: meta,
+        iconBg: "#a2d2ff",
+        date: "Since 2023",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "React.js Developer",
+        company_name: "Techaton",
+        icon: starbucks,
+        iconBg: "#accbe1",
+        date: "2021",
+        points: [
+            "Developing and maintaining web applications using React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "React Native Developer",
+        company_name: "TIIDELab",
+        icon: tesla,
+        iconBg: "#fbc3bc",
+        date: "2022",
+        points: [
+            "Developing and maintaining web applications using React Native and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    },
+    {
+        title: "Web Developer",
+        company_name: "Freelancing",
+        icon: shopify,
+        iconBg: "#b7e4c7",
+        date: "2021 - Present",
+        points: [
+            "Developing and maintaining web applications using Next js / React.js and other related technologies.",
+            "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+            "Implementing responsive design and ensuring cross-browser compatibility.",
+            "Participating in code reviews and providing constructive feedback to other developers.",
+        ],
+    }
+];
+
+export const socialLinks = [
+    
+     {
+        name: 'Contact',
+        iconUrl: contact,
+        link: '/contact',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/Talk2David1',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/david-titus-a6a926185?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    },
+    {
+        name: 'Whatsapp',
+        iconUrl: whatsapp,
+        link: 'https://wa.me/2348166175255', 
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: summiz,
+        theme: 'btn-back-red',
+        name: 'Mobile Fitness App',
+        description: 'Developed a mobile fitness application using React Native, designed to help users achieve their fitness goals, and monitor progress.',
+        link: 'https://appetize.io/app/hqwn5waoahwv34z7xqmcftzk7i?device=pixel8&osVersion=14.0&toolbar=true',
+    },
+    {
+        iconUrl: estate,
+        theme: 'btn-back-black',
+        name: 'Real-Estate Application',
+        description: 'Developed a web application for real estate listings, facilitating property searches and connecting buyers with sellers.',
+        link: 'https://expo.dev/preview/update?message=real%20estate%20app&updateRuntimeVersion=1.0.0&createdAt=2025-04-28T12%3A58%3A16.424Z&slug=exp&projectId=f9320166-bc74-488b-9bbc-b4355a42a12a&group=01682c87-e96c-417d-92f0-4ac3ed8fb36a',
+    },
+    {
+        iconUrl: snapgram,
+        theme: 'btn-back-pink',
+        name: 'Loopstudios',
+        description: 'Landing page for Loop Studios, a leader in interactive VR experiences that blends cutting-edge technology with engaging interactive elements.',
+        link: 'https://responsive-loopstudio.netlify.app/',
+    },
+    {
+        iconUrl:pricewise,
+        theme: 'btn-back-yellow',
+        name: 'Portfolio Website',
+        description: 'Built a Portfolio website for a Data Analyst which led to more exposure and a 30% boost in career prospect and being visible to prospective employers',
+        link: 'https://talk2david1.github.io/David_Portfolio_Website/',
+    }
+    // {
+    //     iconUrl: pricewise,
+    //     theme: 'btn-back-red',
+    //     name: 'Amazon Price Tracker',
+    //     description: 'Developed a web application that tracks and notifies users of price changes for products on Amazon, helping users find the best deals.',
+    //     link: 'https://github.com/adrianhajdin/pricewise',
+    // },
+    // {
+    //     iconUrl: threads,
+    //     theme: 'btn-back-green',
+    //     name: 'Full Stack Threads Clone',
+    //     description: 'Created a full-stack replica of the popular discussion platform "Threads," enabling users to post and engage in threaded conversations.',
+    //     link: 'https://github.com/adrianhajdin/threads',
+    // },
+    // {
+    //     iconUrl: car,
+    //     theme: 'btn-back-blue',
+    //     name: 'Car Finding App',
+    //     description: 'Designed and built a mobile app for finding and comparing cars on the market, streamlining the car-buying process.',
+    //     link: 'https://github.com/adrianhajdin/project_next13_car_showcase',
+    // },
+     // {
+    //     iconUrl: snapgram,
+    //     theme: 'btn-back-pink',
+    //     name: 'Full Stack Instagram Clone',
+    //     description: 'Built a complete clone of Instagram, allowing users to share photos and connect with friends in a familiar social media environment.',
+    //     link: 'https://responsive-loopstudio.netlify.app/',
+    // },
+];
