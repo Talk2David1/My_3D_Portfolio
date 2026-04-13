8ff790a95263d5363250e9baa7bfeee0f521c1dd
David Ezekiel <david@axumtechnologies.com>
2026-04-13 16:02:07 +0100

    ng

--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -4,22 +4,22 @@ import { useGLTF, useAnimations } from "@react-three/drei";
 import planeScene from "../assets/3d/plane.glb";
 
 // 3D Model from: https://sketchfab.com/3d-models/stylized-ww1-plane-c4edeb0e410f46e8a4db320879f0a1db
-export function Plane({ isRotating, ...props }) {
+export function Plane({ ...props }) {
   const ref = useRef();
   // Load the 3D model and its animations
   const { scene, animations } = useGLTF(planeScene);
   // Get animation actions associated with the plane
   const { actions } = useAnimations(animations, ref);
 
-  // Use an effect to control the plane's animation based on 'isRotating'
+  // Keep plane animation running continuously.
   // Note: Animation names can be found on the Sketchfab website where the 3D model is hosted.
   useEffect(() => {
-    if (isRotating) {
-      actions["Take 001"].play();
-    } else {
-      actions["Take 001"].stop();
-    }
-  }, [actions, isRotating]);
+    actions["Take 001"]?.play();
+
+    return () => {
+      actions["Take 001"]?.stop();
+    };
+  }, [actions]);
 
   return (
     <mesh {...props} ref={ref}>
